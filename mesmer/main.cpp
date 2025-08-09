ce07efd77ed692e4d598db43d37fd8d6fe29c9ca
Titan <68808479+titan3755@users.noreply.github.com>
2025-08-09 19:43:15 +0600

    removed comments (this is only sample code)

--- mesmer/main.cpp
+++ mesmer/main.cpp
@@ -1,29 +1,21 @@
 #include <iostream>
 #include <stdexcept>
 
-// Use GLAD to load OpenGL functions
 #include <glad/glad.h>
-
-// Use SDL2 for windowing and input
 #include <SDL.h>
 #include <SDL_opengl.h>
-
-// Use ImGui for the user interface
 #include "imgui.h"
 #include "imgui_impl_sdl2.h"
 #include "imgui_impl_opengl3.h"
 
-// Screen dimensions
-const int SCREEN_WIDTH = 1280;
-const int SCREEN_HEIGHT = 720;
+const int SCREEN_WIDTH = 1920;
+const int SCREEN_HEIGHT = 1080;
 
 void cleanup(SDL_Window* window) {
-    // Cleanup ImGui
     ImGui_ImplOpenGL3_Shutdown();
     ImGui_ImplSDL2_Shutdown();
     ImGui::DestroyContext();
 
-    // Destroy window and quit SDL
     if (window) {
         SDL_DestroyWindow(window);
     }
@@ -34,19 +26,16 @@ int main(int argc, char* argv[]) {
     SDL_Window* window = nullptr;
 
     try {
-        // --- 1. Initialize SDL and Create a Window ---
         if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER | SDL_INIT_GAMECONTROLLER) != 0) {
             throw std::runtime_error(std::string("SDL_Init Error: ") + SDL_GetError());
         }
 
-        // Set OpenGL attributes
         const char* glsl_version = "#version 460";
         SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, 0);
         SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
         SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
         SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 6);
 
-        // Create a window with an OpenGL context
         SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
         SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
         SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
@@ -69,9 +58,8 @@ int main(int argc, char* argv[]) {
             throw std::runtime_error(std::string("SDL_GL_CreateContext Error: ") + SDL_GetError());
         }
         SDL_GL_MakeCurrent(window, gl_context);
-        SDL_GL_SetSwapInterval(1); // Enable vsync
+        SDL_GL_SetSwapInterval(1);
 
-        // --- 2. Initialize GLAD ---
         if (!gladLoadGLLoader((GLADloadproc)SDL_GL_GetProcAddress)) {
             throw std::runtime_error("Failed to initialize GLAD.");
         }
@@ -80,36 +68,29 @@ int main(int argc, char* argv[]) {
         std::cout << "Renderer: " << glGetString(GL_RENDERER) << std::endl;
         std::cout << "Version:  " << glGetString(GL_VERSION) << std::endl;
 
-        // --- 3. Initialize ImGui ---
         IMGUI_CHECKVERSION();
         ImGui::CreateContext();
         ImGuiIO& io = ImGui::GetIO(); (void)io;
-        io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard; // Enable Keyboard Controls
-        io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;     // Enable Docking
-        io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;   // Enable Multi-Viewport / Platform Windows
+        io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+        io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
+        io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;
 
-        // Setup Dear ImGui style
         ImGui::StyleColorsDark();
-
-        // When viewports are enabled we tweak WindowRounding/WindowBg so platform windows can look identical to regular ones.
         ImGuiStyle& style = ImGui::GetStyle();
         if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable) {
             style.WindowRounding = 0.0f;
             style.Colors[ImGuiCol_WindowBg].w = 1.0f;
         }
 
-        // Setup Platform/Renderer backends
         ImGui_ImplSDL2_InitForOpenGL(window, gl_context);
         ImGui_ImplOpenGL3_Init(glsl_version);
 
-        // --- 4. Main Loop ---
         bool done = false;
         ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
         bool show_demo_window = true;
         bool show_another_window = false;
 
         while (!done) {
-            // Poll and handle events (inputs, window resize, etc.)
             SDL_Event event;
             while (SDL_PollEvent(&event)) {
                 ImGui_ImplSDL2_ProcessEvent(&event);
@@ -121,26 +102,24 @@ int main(int argc, char* argv[]) {
                 }
             }
 
-            // Start the Dear ImGui frame
             ImGui_ImplOpenGL3_NewFrame();
             ImGui_ImplSDL2_NewFrame();
             ImGui::NewFrame();
 
-            // Show a simple window that we create ourselves. We use a Begin/End pair to create a named window.
             {
                 static float f = 0.0f;
                 static int counter = 0;
 
-                ImGui::Begin("Hello, world!"); // Create a window called "Hello, world!" and append into it.
+                ImGui::Begin("Hello, world!");
 
-                ImGui::Text("This is some useful text.");               // Display some text
-                ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
+                ImGui::Text("This is some useful text.");
+                ImGui::Checkbox("Demo Window", &show_demo_window);
                 ImGui::Checkbox("Another Window", &show_another_window);
 
-                ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
-                ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
+                ImGui::SliderFloat("float", &f, 0.0f, 1.0f);
+                ImGui::ColorEdit3("clear color", (float*)&clear_color);
 
-                if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
+                if (ImGui::Button("Button"))
                     counter++;
                 ImGui::SameLine();
                 ImGui::Text("counter = %d", counter);
@@ -149,30 +128,24 @@ int main(int argc, char* argv[]) {
                 ImGui::End();
             }
 
-            // Show another simple window.
             if (show_another_window) {
-                ImGui::Begin("Another Window", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
+                ImGui::Begin("Another Window", &show_another_window);
                 ImGui::Text("Hello from another window!");
                 if (ImGui::Button("Close Me"))
                     show_another_window = false;
                 ImGui::End();
             }
 
-            // Show the ImGui demo window.
             if (show_demo_window) {
                 ImGui::ShowDemoWindow(&show_demo_window);
             }
 
-            // --- 5. Rendering ---
             ImGui::Render();
             glViewport(0, 0, (int)io.DisplaySize.x, (int)io.DisplaySize.y);
             glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w);
             glClear(GL_COLOR_BUFFER_BIT);
 
-            // Render ImGui draw data
             ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-
-            // Update and Render additional Platform Windows
             if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable) {
                 SDL_Window* backup_current_window = SDL_GL_GetCurrentWindow();
                 SDL_GLContext backup_current_context = SDL_GL_GetCurrentContext();
@@ -191,7 +164,6 @@ int main(int argc, char* argv[]) {
         return -1;
     }
 
-    // --- 6. Cleanup ---
     cleanup(window);
     return 0;
 }
