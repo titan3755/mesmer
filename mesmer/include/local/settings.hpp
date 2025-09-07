ddd2337e49f7bb865bbb4fb0fee706789c48cc27
Titan <68808479+titan3755@users.noreply.github.com>
2025-09-07 10:14:37 +0600

    updated settings.hpp

--- mesmer/include/local/settings.hpp
+++ mesmer/include/local/settings.hpp
@@ -4,11 +4,25 @@
 
 #include <iostream>
 #include <fstream>
+#include <string>
+#include <map>
+#include <spdlog/spdlog.h>
 
 class Settings {
 public:
 	Settings(const char* filename);
 	~Settings();
+
+	bool exists() const { return fileExists; }
+
+private:
+	bool fileExists = false;
+	std::string filename;
+	std::map<std::string, std::string> settingsMap;
+
+	void loadSettings();
+	void appendSettings(std::map<std::string, std::string>);
+
 };
 
 #endif
