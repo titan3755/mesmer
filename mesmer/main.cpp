0fff93cdf4cf0544dd6cca06a033304945ff8ae3
Titan <68808479+titan3755@users.noreply.github.com>
2025-09-13 11:40:10 +0600

    added intro comments in main.cpp

--- mesmer/main.cpp
+++ mesmer/main.cpp
@@ -1,3 +1,8 @@
+/*
+	Mesmer - An interactive and high-performance fractal generator and explorer.
+	Made by -> Titan // GH: https://github.com/titan3755/mesmer
+*/
+
 #include <application.hpp>
 
 const char* WINDOW_TITLE = "Mesmer";
