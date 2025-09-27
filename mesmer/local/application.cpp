7ebfd92aa46cea5caa2e8900ba36c071a3c9b72d
Titan <68808479+titan3755@users.noreply.github.com>
2025-09-27 13:46:35 +0600

    added the pre render checkbox

--- mesmer/local/application.cpp
+++ mesmer/local/application.cpp
@@ -1203,6 +1203,7 @@ void Application::run() {
 					}
 				}
 				ImGui::Checkbox("Toggle info window", &info_gui_window_toggle);
+				ImGui::Checkbox("Toggle pre-rendering system", &m_pre_render_enabled);
 				ImGui::End();
 			}
 
