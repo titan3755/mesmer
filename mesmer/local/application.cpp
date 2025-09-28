4f67aced7ac02c1521eecdeafa85202b5b15c859
Titan <68808479+titan3755@users.noreply.github.com>
2025-09-28 14:06:15 +0600

    modded reset logic

--- mesmer/local/application.cpp
+++ mesmer/local/application.cpp
@@ -219,6 +219,16 @@ void Application::run() {
 						m_drag_start_pos = current_pos;
 					}
 					if (event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_SPACE) {
+						if (m_pre_render_complete) {
+							glDeleteTextures(1, &m_pre_render_texture);
+							glDeleteFramebuffers(1, &m_pre_render_fbo);
+							delete m_texture_view_shader;
+							m_pre_render_texture = 0;
+							m_pre_render_fbo = 0;
+							m_texture_view_shader = nullptr;
+						}
+						m_pre_render_enabled = false;
+						m_pre_render_complete = false;
 						show_main_buttons = !show_main_buttons;
 						show_fractal_selection = false;
 						spdlog::info("'Space' key pressed - toggling main buttons.");
