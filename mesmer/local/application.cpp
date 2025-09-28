76978387ddc08b10acf389fda1f6b6bfc91dd6ca
Titan <68808479+titan3755@users.noreply.github.com>
2025-09-28 19:56:21 +0600

    pre render bug fixing one

--- mesmer/local/application.cpp
+++ mesmer/local/application.cpp
@@ -2115,7 +2115,7 @@ void Application::performPreRender() {
 	glBindFramebuffer(GL_FRAMEBUFFER, m_pre_render_fbo);
 	glGenTextures(1, &m_pre_render_texture);
 	glBindTexture(GL_TEXTURE_2D, m_pre_render_texture);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA32F, m_pre_render_resolution, m_pre_render_resolution, 0, GL_RGBA, GL_FLOAT, NULL);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, m_pre_render_resolution, m_pre_render_resolution, 0, GL_RGBA, GL_UNSIGNED_BYTE, NULL);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, m_pre_render_texture, 0);
